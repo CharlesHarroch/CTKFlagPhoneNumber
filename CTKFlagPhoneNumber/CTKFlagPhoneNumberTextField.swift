a7fbeb773ece7ae3be87c5421798c172876c1a54
Charles HARROCH <charles@awayup.io>
2018-12-27 10:30:40 +0100

    udpate to swift 4.2

--- CTKFlagPhoneNumber/CTKFlagPhoneNumberTextField.swift
+++ CTKFlagPhoneNumber/CTKFlagPhoneNumberTextField.swift
@@ -85,7 +85,7 @@ open class CTKFlagPhoneNumberTextField: UITextField, UITextFieldDelegate, Countr
 	
 	private func setup() {
 		borderStyle = .roundedRect
-		leftViewMode = UITextFieldViewMode.always
+        leftViewMode = UITextField.ViewMode.always
 		autocorrectionType = .no
 		addTarget(self, action: #selector(didEditText), for: .editingChanged)
 		delegate = self
@@ -145,7 +145,7 @@ open class CTKFlagPhoneNumberTextField: UITextField, UITextFieldDelegate, Countr
 	}
 	
 	@objc private func displayNumberKeyBoard() {
-		keyboardType = .numberPad
+		keyboardType = .phonePad
 		inputView = nil
 		inputAccessoryView = nil
 		tintColor = .gray
@@ -165,7 +165,7 @@ open class CTKFlagPhoneNumberTextField: UITextField, UITextFieldDelegate, Countr
 	}
 	
 	@objc private func resetKeyBoard() {
-		keyboardType = .default
+		keyboardType = .phonePad
 		inputView = nil
 		inputAccessoryView = nil
 		resignFirstResponder()
@@ -239,13 +239,13 @@ open class CTKFlagPhoneNumberTextField: UITextField, UITextFieldDelegate, Countr
 	}
 
 	private func getCountryListBarButtonItems() -> [UIBarButtonItem] {
-		let space = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
-		let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(resetKeyBoard))
+        let space = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(resetKeyBoard))
 		
 		doneButton.accessibilityLabel = "doneButton"
 		
 		if parentViewController != nil {
-			let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.search, target: self, action: #selector(displayAlphabeticKeyBoard))
+            let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.search, target: self, action: #selector(displayAlphabeticKeyBoard))
 			
 			searchButton.accessibilityLabel = "searchButton"
 			
